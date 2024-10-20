198b464a36eb67e8813e13b29e5b2d3244d4f8c0
Andres-xrg <edwinandresrangelgomez8@gmail.com>
2024-10-20 17:45:20 -0500

    Update script.js

--- Mi portafolio/js/script.js
+++ Mi portafolio/js/script.js
@@ -1,5 +1,5 @@
 var typed =new Typed(".typing", {
-    strings:["","Web Designer","Web Developer", "Web Programmer"],
+    strings:["","Web Developer", "Web Programmer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
